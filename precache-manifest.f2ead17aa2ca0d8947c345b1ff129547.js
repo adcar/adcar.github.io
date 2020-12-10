21249d3ca36d63c1049ce3d7caa3c8707f5b3f27
adcar <adcar@github.com>
2020-12-10 15:34:45 -0500

    initial

---
+++ precache-manifest.f2ead17aa2ca0d8947c345b1ff129547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6e89e2c70f5b8535104b3ffc65316f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62161da4327e5e02b3db",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "230b403496c1ff955185",
+    "url": "/static/js/2.f2c4fa96.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/static/js/2.f2c4fa96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62161da4327e5e02b3db",
+    "url": "/static/js/main.48c25364.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "52d01ae8a5c786a1b594da08e7280c0f",
+    "url": "/static/media/env.52d01ae8.svg"
+  }
+]);
